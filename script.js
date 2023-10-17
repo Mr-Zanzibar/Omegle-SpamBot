ed4fa6a7a7a24fdfca88acb527f789a0e5a9d34d
Mr-Zanzibar <104436099+Mr-Zanzibar@users.noreply.github.com>
2023-10-17 19:28:28 +0200

    anti ban :)
    not a real antiban, just make sure some nerd doesn’t use it in the video section

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ let retryCount = 0;
 
 function Spam() {
     try {
+        let selfVideo = document.getElementById('selfvideo'); // you can be banned if you use this script in the video section
+        if (selfVideo) {
+            console.warn("SCRIPT CAN ONLY BE USED IN THE TEXT SECTION OF OMEGLE");
+            console.error("THIS IS TO PREVENT NERDS LIKE YOU FROM BEING BANNED");
+            clearInterval(spamInterval);
+            return;
+        }
+
         var wlc = ['Hi', 'Hey', 'Sup', 'Hello', 'lol', ':)', 'Whats up', 'Yo', 'Heyy'];
         var Ad = ['YOUR_MESSAGE', 'YOUR_MESSAGE'];
 
         let sendButton = document.querySelector('.sendbtn');
         let disconnectButton = document.querySelector('.disconnectbtn');
         let chatMessage = document.querySelector('.chatmsg');
-
         let broadcastId = document.getElementById('broadcast-id').textContent;
-
         let onlineCount = document.getElementById('onlinecount').textContent.trim();
 
         if (!sendButton || !disconnectButton || !chatMessage) {
@@ -20,6 +26,7 @@ function Spam() {
                 return;
             }
             console.error("One or more elements not found. Aborting spam.");
+            console.log('%cReport this error on the official script page: https://github.com/Mr-Zanzibar/Omegle-SpamBot', 'color: magenta; font-weight: bold;');
             clearInterval(spamInterval);
             return;
         }
